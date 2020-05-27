c82b2c111746efbe40ee259d55c2e562155975f7
zheng-jj <64000939+zheng-jj@users.noreply.github.com>
2020-05-27 09:39:09 +0800

    initial commit

---
+++ app/src/main/java/com/example/mad_practical_6_data_and_file_storage_zheng_jj/week_6_whackamole_3_0/CustomScoreViewHolder.java
@@ -0,0 +1,21 @@
+package com.example.mad_practical_6_data_and_file_storage_zheng_jj.week_6_whackamole_3_0;
+
+import android.view.View;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CustomScoreViewHolder extends RecyclerView.ViewHolder {
+    /* Hint:
+        1. This is a customised view holder for the recyclerView list @ levels selection page
+     */
+    private static final String FILENAME = "CustomScoreViewHolder.java";
+    private static final String TAG = "Whack-A-Mole3.0!";
+
+    public CustomScoreViewHolder(final View itemView){
+        super(itemView);
+
+        /* Hint:
+        This method dictates the viewholder contents and links the widget to the objects for manipulation.
+         */
+    }
+}
---
+++ app/src/main/java/com/example/mad_practical_6_data_and_file_storage_zheng_jj/week_6_whackamole_3_0/Main3Activity.java
@@ -0,0 +1,41 @@
+package com.example.mad_practical_6_data_and_file_storage_zheng_jj.week_6_whackamole_3_0;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main3Activity extends AppCompatActivity {
+    /* Hint:
+        1. This displays the available levels from 1 to 10 to the user.
+        2. The different levels makes use of the recyclerView and displays the highest score
+           that corresponds to the different levels.
+        3. Selection of the levels will load relevant Whack-A-Mole game.
+        4. The levels are with the following difficulties.
+            a. Level 1 will have a new mole at each 10000ms.
+            b. Each level up will shorten the time to next mole by 100ms with level 10 as 1000 second per mole.
+            c. For level 1 ~ 5, there is only 1 mole.
+            d. For level 6 ~ 10, there are 2 moles.
+            e. Each location of the mole is randomised.
+        5. There is an option return to the login page.
+     */
+    private static final String FILENAME = "Main3Activity.java";
+    private static final String TAG = "Whack-A-Mole3.0!";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+        /* Hint:
+        This method receives the username account data and looks up the database for find the
+        corresponding information to display in the recyclerView for the level selections page.
+
+        Log.v(TAG, FILENAME + ": Show level for User: "+ userName);
+         */
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        finish();
+    }
+}
